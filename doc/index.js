a1552d11c916f6067ed278cbd4b01d8d08cb0c92
ajgutierrezr <ajgutierrez@uc.cl>
2018-09-29 18:33:08 -0300

    yay

--- doc/index.js
+++
@@ -1,9 +0,0 @@
-const KoaRouter = require('koa-router');
-const pkg = require('../../../package.json');
-
-const router = new KoaRouter();
-router.get('doc.show', '/', async (ctx) => {
-  await ctx.render('../routes/doc/index', { appVersion: pkg.version });
-});
-
-module.exports = router;
